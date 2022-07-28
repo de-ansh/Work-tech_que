4c9475e4bb64ad333fb0218a2a8924977dd88a14
Ashish Mishra <72343696+de-ansh@users.noreply.github.com>
2022-07-28 23:31:40 +0530

    merge sorted subarrays

---
+++ MergesortedSubarrays.cpp
@@ -0,0 +1,20 @@
+void merge(vector<int> &arr, int endIndex) {
+   int subarr1Current = 0, subarr2Current = endIndex + 1;
+   vector<int> ans;
+   while (subarr1Current <= endIndex && subarr2Current < arr.size()) {
+       if(arr[subarr1Current] < arr[subarr2Current]) {
+           ans.push_back(arr[subarr1Current++]);
+       } else {
+           ans.push_back(arr[subarr2Current++]);
+       }
+   }
+   while (subarr1Current <= endIndex) {
+       ans.push_back(arr[subarr1Current++]);
+   }
+   while (subarr2Current < arr.size()) {
+       ans.push_back(arr[subarr2Current++]);
+   }
+   for (int i = 0; i < arr.size(); i++) {
+       arr[i] = ans[i];
+   }
+}
