31bd0d2d30be01b035f75de0809b816a0932f473
Ashish <anshm9448@gmail.com>
2022-07-30 09:56:50 +0530

    AP

---
+++ Arithmeticprog.cpp
@@ -0,0 +1,14 @@
+bool isArithmeticSequence(vector<int> &arr) {
+    int n = arr.size();
+   if (n == 1) {
+       return true;
+   }
+   sort(arr.begin(), arr.end());
+   int commonDiff = arr[1] - arr[0];
+   for (int i = 1; i < n; i++) {
+       if (arr[i] - arr[i - 1] != commonDiff) {
+           return false;
+       }
+   }
+   return true;
+}
